311720c34c363033813bbe33f0abbc79a6ff0379
Daniel <danielwidegren@gmail.com>
2025-09-15 19:18:39 +0000

    map song data to releases. play song from release

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Music } from './music';
+import { Music, Release, Song } from './music';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  // protected readonly title = signal('dw-homepage');
-  protected audioPath = 'http://localhost:5299/weatherforecast';
+  protected readonly releases = signal<Release[]>([]);
+  protected readonly audioPath = signal<string>('');
+
+  private readonly baseAudioPath = 'http://localhost:5299/play/';
   constructor(private musicService: Music) {}
 
   ngOnInit(): void {
     this.musicService.getData().subscribe({
-      next: (res) => console.log('music data', res),
+      next: (releases) => this.releases.set(releases),
       error: (err) => console.error(err),
     });
+  }
+
+  play(song: Song) {
+    this.audioPath.update(() => `${this.baseAudioPath}${song.fileName}`);
   }
 }
--- src/app/music.ts
+++ src/app/music.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,50 @@ export class Music {
 
   constructor(private http: HttpClient) {}
 
-  getData(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/songs`);
+  getData(): Observable<Release[]> {
+    return this.http.get(`${this.baseUrl}/songs`).pipe(
+      map((data: any) => {
+        if (!this.isValidData(data)) {
+          console.error('Invalid music data received.');
+          return [];
+        } else {
+          return this.mapSongsToReleases(data.data);
+        }
+      })
+    );
   }
+
+  private isValidData(data: any) {
+    return ('data' in data) && Array.isArray(data.data);
+  }
+
+  private mapSongsToReleases(songs: any[]) {
+    return songs.reduce((result, song) => {
+      let release = result.find((r: Release) => r.name === song.release);
+      if (!release) {
+        release = new Release(song.release, song.artist);
+        result.push(release);
+      }
+      release.songs.push(new Song(song.name, song.information, song.fileName, song.trackNumber));
+      release.songs.sort((a: Song, b: Song) => a.trackNumber - b.trackNumber);
+      return result;
+    }, []);
+  }
+}
+
+export class Release {
+  constructor(
+    public name: string,
+    public artist: string,
+    public songs: Song[] = [],
+  ) {}
+}
+
+export class Song {
+  constructor(
+    public name: string,
+    public information: string,
+    public fileName: string,
+    public trackNumber: number,
+  ) {}
 }
