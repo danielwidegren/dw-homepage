483aac9d8e49a1cbb53be237b53c0adc664fdbd7
Daniel <danielwidegren@gmail.com>
2025-09-17 19:26:57 +0000

    add play for release. play song or first song in release after button click

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Music, Release, Song } from './music';
 import { CommonModule } from '@angular/common';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,22 @@ import { CommonModule } from '@angular/common';
 export class App {
   protected readonly releases = signal<Release[]>([]);
   protected readonly audioPath = signal<string>('');
+  protected readonly playlist = signal<Song[]>([]);
+  protected readonly currentSong = signal<Song | null>(null);
+  @ViewChild('audioPlayer') audioPlayer?: ElementRef<HTMLAudioElement>;
 
   private readonly baseAudioPath = 'http://localhost:5299/play/';
-  constructor(private musicService: Music) {}
+  constructor(private musicService: Music) {
+    toObservable(this.currentSong).subscribe(() => {
+      setTimeout(() => {
+        if (this.audioPlayer) {
+          this.audioPlayer.nativeElement.play()
+            .then(() => {})
+            .catch(error => console.error('Audio player error', error));
+        }
+      }, 400);
+    });
+  }
 
   ngOnInit(): void {
     this.musicService.getData().subscribe({
@@ -23,7 +37,27 @@ export class App {
     });
   }
 
-  play(song: Song) {
+  playSong(song: Song) {
+    this.playlist.update(() => [song]);
+    this.startSong(song);
+  }
+
+  playRelease(release: Release) {
+    this.playlist.update(() => release.songs);
+    this.startSong(release.songs[0]);
+  }
+
+  startSong(song: Song) {
+    this.currentSong.update(() => song);
     this.audioPath.update(() => `${this.baseAudioPath}${song.fileName}`);
+  }
+
+  songEnded() {
+    if (this.playlist().length > 1) {
+      const songIndex = this.playlist().findIndex(s => s.fileName === this.currentSong()!.fileName);
+      if (songIndex !== this.playlist().length - 1) {
+        this.startSong(this.playlist()[songIndex + 1]);
+      }
+    }
   }
 }
